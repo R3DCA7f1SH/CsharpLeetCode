e716f0e349131dd1c81720904be06cc9c7169d6b
R3DCA7f1SH <mikhailodinol@gmail.com>
2024-07-22 19:59:18 +0700

    BST sum

---
+++ CsharpLeetCode.Main/Medium/BST_ToGreaterSumTree.cs
@@ -0,0 +1,64 @@
+﻿namespace CsharpLeetCode.Main.Medium
+{
+    using CsharpLeetCode.Main.Medium.DataStructure;
+    
+    // Задача: https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree
+    public class BST_ToGreaterSumTree
+    {
+        private static Dictionary<int,int> GetSumMap(TreeNode node, Dictionary<int, int> accum)
+        {
+            if (node == null) return accum;
+            var newDict = new Dictionary<int, int>(accum);
+            newDict.Add(node.val,node.val);
+            foreach(var item in newDict)
+            {
+                var currentKey = item.Key;
+                var currentValue = item.Value;
+
+                var keys = newDict.Keys.Where(s=>s>item.Key);
+                newDict[currentKey] = keys.Sum();
+            }
+            newDict = GetSumMap(node.left, newDict);
+            newDict = GetSumMap(node.right, newDict);
+            return newDict;
+        }
+       
+
+        private static TreeNode UpdateBstByDict(TreeNode node, Dictionary<int, int> accum)
+        {
+            if (node == null) return null;
+            node.val += accum[node.val];
+            UpdateBstByDict(node.left, accum);
+            UpdateBstByDict(node.right, accum);
+            return node;
+        }
+
+        public TreeNode BstToGst(TreeNode root) 
+        { 
+            var newDict = new Dictionary<int,int>();
+            var updatedDict = GetSumMap(root, newDict);            
+            var result = UpdateBstByDict(root, updatedDict);
+            return result;            
+        }
+
+        //return UpdateBstBySum(root, SumAllBstNode(root)); 
+    }
+
+    public class BST_ToGreaterSumTree_Optimized
+    {
+        public TreeNode BstToGst(TreeNode root)
+        {
+            int max = 0;
+            CheckInOrder(root,ref max);
+            return root;
+        }
+
+        private static void CheckInOrder(TreeNode n, ref int max)
+        {
+            if (n == null) return;
+            CheckInOrder(n.right, ref max); // Обход справа налево, от больших значений к меньшим
+            max = n.val += max; // Максимуму присваивается сумма текущего узла с предыдущим максимумом
+            CheckInOrder(n.left, ref max);
+        }
+    }
+}
---
+++ CsharpLeetCode.Tests/Medium/BST_ToGreaterSumTreeTests.cs
@@ -0,0 +1,59 @@
+﻿using CsharpLeetCode.Main.Medium;
+using CsharpLeetCode.Main.Medium.DataStructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpLeetCode.Tests.Medium
+{
+    public  class BST_ToGreaterSumTreeTests
+    {
+        private readonly BST_ToGreaterSumTree _ToGreaterSumTree = new();
+        [Fact]
+        public void FirstCase()
+        {
+            var oldRoot = new TreeNode()
+            {
+                val = 4,
+                left=new TreeNode()
+                {
+                    val=1,
+                    left=new TreeNode()
+                    {
+                        val = 0
+                    },
+                    right=new TreeNode()
+                    {
+                        val=2,
+                        right=new TreeNode()
+                        {
+                            val=3
+                        }
+                    }
+                },
+                right=new TreeNode()
+                {
+                    val=6,
+                    left=new TreeNode()
+                    {
+                        val = 5
+                    },
+                    right = new TreeNode()
+                    {
+                        val=7,
+                        right=new TreeNode()
+                        {
+                            val=8
+                        }
+                    }
+                }
+            };
+
+            var newRoot = _ToGreaterSumTree.BstToGst(oldRoot);
+        }
+    }
+
+    
+}
