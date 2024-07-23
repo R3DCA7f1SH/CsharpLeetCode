c76ebc3d7831c7fd39180d9d9b8feaf3807d79d6
R3DCA7f1SH <mikhailodinol@gmail.com>
2024-07-23 22:33:08 +0700

    Сортировка по частоте появления в массиве

---
+++ CsharpLeetCode.Main/Easy/SortArrayByIncreasingFrequency.cs
@@ -0,0 +1,38 @@
+﻿namespace CsharpLeetCode.Main.Easy;
+public class SortArrayByIncreasingFrequency
+{
+    private static Dictionary<int,int> GetFreqMap(int[] nums)
+    {
+        var mapFreq = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (mapFreq.ContainsKey(nums[i]))
+            {
+                mapFreq[nums[i]]++;
+                continue;
+            }
+            mapFreq.Add(nums[i], 1);
+        }
+        return mapFreq;
+    }
+    public int[] FrequencySort(int[] nums)
+    {
+        var mapFreq = GetFreqMap(nums);
+        for(int i = 0;i < nums.Length; i++)
+        {
+            bool isSwaped = false;
+            for (int j = 0; j < nums.Length - 1 - i; j++)
+            {
+                if (mapFreq[nums[j]] > mapFreq[nums[j + 1]] 
+                    || (mapFreq[nums[j]] == mapFreq[nums[j + 1]] && nums[j] < nums[j + 1]))
+                {
+                    (nums[j + 1], nums[j]) = (nums[j], nums[j + 1]);
+                    isSwaped = true;
+                }
+            }
+            if (!isSwaped) break;
+        }
+        return nums;
+    }
+}
+
---
+++ CsharpLeetCode.Tests/Easy/SortArrayByIncreasingFrequencyTests.cs
@@ -0,0 +1,35 @@
+﻿using CsharpLeetCode.Main.Easy;
+
+namespace CsharpLeetCode.Tests.Easy;
+public class SortArrayByIncreasingFrequencyTests
+{
+    private readonly SortArrayByIncreasingFrequency _sortArrayByIncreasingFrequency = new();
+    [Fact]
+    public void CaseN1()
+    {
+        var nums = new int[] { 1, 1, 2, 2, 2, 3 };
+        var expected = new int[] { 3, 1, 1, 2, 2, 2 };
+        var result = _sortArrayByIncreasingFrequency.FrequencySort(nums);
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void CaseN2()
+    {
+        var nums = new int[] { 2, 3, 1, 3, 2 };
+        var expected = new int[] { 1, 3, 3, 2, 2 };
+        var result = _sortArrayByIncreasingFrequency.FrequencySort(nums);
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void CaseN3()
+    {
+        var nums = new int[] { -1, 1, -6, 4, 5, -6, 1, 4, 1 };
+        var expected = new int[] { 5, -1, 4, 4, -6, -6, 1, 1, 1 };
+        var result = _sortArrayByIncreasingFrequency.FrequencySort(nums);
+        Assert.Equal(expected, result);
+    }
+
+}
+
