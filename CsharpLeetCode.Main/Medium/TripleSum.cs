53c71a67d3add34cdc2fc87e67c3c2758fa8c9dd
R3DCA7f1SH <mikhailodinol@gmail.com>
2024-07-23 16:53:44 +0700

    TripleSum

---
+++ CsharpLeetCode.Main/Medium/TripleSum.cs
@@ -0,0 +1,82 @@
+﻿using System.Diagnostics;
+
+namespace CsharpLeetCode.Main.Medium;
+public class MyTripleSum
+{
+    private static List<int[]> TwoSum(int[] nums, int target,int exceptedIndex)
+    {
+        var map = new Dictionary<int, int>();
+        var result = new List<int[]>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (i == exceptedIndex) continue;
+
+            int otherNumber = target - nums[i];
+            if (map.ContainsKey(otherNumber)) result.Add(new int[] { otherNumber, nums[i] });
+            else map.TryAdd(nums[i], i);
+        }
+        return result;
+    }
+
+    public IList<IList<int>> ThreeSum(int[] nums)
+    {
+        var result = new List<IList<int>>();
+        var map = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (map.ContainsKey(nums[i])) continue;
+            map.Add(nums[i], i);
+            
+            var target = 0 - nums[i];                       
+            var twoSumCheck = TwoSum(nums, target,i);
+            if (twoSumCheck.Count == 0) continue;
+
+            foreach (var doubles in twoSumCheck)
+            {
+                var toAdd = new List<int>(doubles) { nums[i] }.ToArray();
+                Array.Sort(toAdd);
+                var isExisted = false;
+                foreach (var list in result)
+                {
+                    isExisted = Enumerable.SequenceEqual(toAdd, list.ToArray());
+                    if(isExisted) break;
+                }
+                if (isExisted) continue;
+                result.Add(toAdd.ToList());
+            }
+        }
+        return result;
+    }
+}
+
+public class TripleSum // По чужому решению
+{
+    public IList<IList<int>> ThreeSum(int[] nums)
+    {
+        var result = new List<IList<int>>();
+        Array.Sort(nums);
+        var length = nums.Length;
+        for (int i = 0; i < length; i++)
+        {
+            if(i>0 && nums[i] == nums[i-1]) continue;
+
+            int left = i + 1;
+            int right = length - 1;
+
+            while (left < right)
+            {
+                var checkSum = nums[i] + nums[left] + nums[right];
+                if (checkSum > 0) right--;
+                else if (checkSum < 0) left++;
+                else
+                {
+                    result.Add(new List<int> { nums[i], nums[left], nums[right] });
+                    left++;
+                    while (nums[left] == nums[left - 1] && left < right) left++;
+                }
+            }
+        }
+        return result;
+    }
+}
+
---
+++ CsharpLeetCode.Tests/Medium/TripleSumTests.cs
@@ -0,0 +1,105 @@
+﻿using CsharpLeetCode.Main.Medium;
+
+namespace CsharpLeetCode.Tests.Medium
+{
+    public class TripleSumTests
+    {
+        private readonly TripleSum tripleSum = new();
+
+        [Fact]
+        public void FirstCase()
+        {
+            var nums = new int[] { -1, 0, 1, 2, -1, -4 };
+            var exceptedNums = new List<List<int>>()
+            {
+                new List<int> { -1, -1, 2 },
+                new List<int> { -1,0,1 },
+            };
+            var resultNums = tripleSum.ThreeSum(nums);
+            Assert.Equal(exceptedNums.Count, resultNums.Count);
+            //Assert.Equal(exceptedNums, resultNums);
+        }
+
+        [Fact]
+        public void SecondCase()
+        {
+            var nums = new int[] { 0, 1, 1 };
+            var exceptedNums = new List<List<int>>();
+            var resultNums = tripleSum.ThreeSum(nums);
+            Assert.Equal(exceptedNums.Count, resultNums.Count);
+            //Assert.Equal(exceptedNums, resultNums);
+        }
+
+        [Fact]
+        public void ThirdCase()
+        {
+            var nums = new int[] { 0, 0, 0 };
+            var exceptedNums = new List<List<int>>()
+            {
+                new List<int>{ 0, 0, 0 }
+            };
+            var resultNums = tripleSum.ThreeSum(nums);
+            Assert.Equal(exceptedNums.Count, resultNums.Count);
+            //Assert.Equal(exceptedNums, resultNums);
+        }
+
+
+        [Fact]
+        public void FourCase()
+        {
+            var nums = new int[] { -1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4 };
+            var exceptedNums = new List<List<int>>()
+            {
+                new List<int>{ -4, 0, 4 },
+                new List<int>{ -4, 1, 3 },
+                new List<int>{ -3, -1, 4 },
+                new List<int>{ -3, 0, 3 },
+                new List<int>{ -3, 1, 2 },
+                new List<int>{ -2, -1, 3 },
+                new List<int>{ -2, 0, 2 },
+                new List<int>{ -1, -1, 2 },
+                new List<int>{ -1, 0, 1 },
+            };
+            var resultNums = tripleSum.ThreeSum(nums);
+            Assert.Equal(exceptedNums.Count, resultNums.Count);
+            //Assert.Equal(exceptedNums, resultNums);
+        }
+
+        [Fact]
+        public void FiveCase()
+        {
+            var nums = new int[] { -5, 0, -2, 3, -2, 1, 1, 3, 0, -5, 3, 3, 0, -1 };
+            var exceptedNums = new List<List<int>>()
+            {
+                new List<int>{ -2, -1, 3},
+                new List<int>{ -2, 1, 1 },
+                new List<int>{ -1, 0, 1 },
+                new List<int>{ 0, 0, 0 }
+            };
+            var resultNums = tripleSum.ThreeSum(nums);
+            Assert.Equal(exceptedNums.Count, resultNums.Count);
+            //Assert.Equal(exceptedNums, resultNums);
+        }
+
+        [Fact]
+        public void SixCase()
+        {
+            var nums = new int[] { -4, -2, 1, -5, -4, -4, 4, -2, 0, 4, 0, -2, 3, 1, -5, 0 };
+            var exceptedNums = new List<List<int>>()
+            {
+                new List<int>{ -5, 1, 4},
+                new List<int>{ -4, 0, 4 },
+                new List<int>{ -4, 1, 3 },
+                new List<int>{ -2, -2, 4 },
+                new List<int>{ -2, 1, 1 },
+                new List<int>{ 0, 0, 0 },
+            };
+            var resultNums = tripleSum.ThreeSum(nums);
+            Assert.Equal(exceptedNums.Count, resultNums.Count);
+            //Assert.Equal(exceptedNums, resultNums);
+        }
+    }
+
+
+    //[[-4,0,4],[-4,1,3],[-3,-1,4],[-3,0,3],[-3,1,2],[-2,-1,3],[-2,0,2],[-1,-1,2],[-1,0,1]]...
+}
