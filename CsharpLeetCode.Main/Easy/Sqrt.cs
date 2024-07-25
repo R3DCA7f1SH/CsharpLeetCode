85b5008217f9c3d14aae6d2c4db3d0814c1a8ff9
R3DCA7f1SH <mikhailodinol@gmail.com>
2024-07-25 16:19:35 +0700

    sqrt

---
+++ CsharpLeetCode.Main/Easy/Sqrt.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace CsharpLeetCode.Main.Easy;
+
+public class Sqrt
+{
+
+    private static float FastInvSqrt(float x)
+    {
+        float xhalf = 0.5f * x;
+        int i = BitConverter.SingleToInt32Bits(x);
+        i = 0x5f3759df - (i >> 1); // magic
+        x = BitConverter.Int32BitsToSingle(i);
+        for (int j = 0; j < 10; j++) x *= (1.5f - (xhalf * x * x));// for accuracy
+        return x;
+    }
+    // Красивое решение, не проходящее в литкоде из-за отдельных случаев
+    private static int MagicSqrt(int x)
+    {
+        if (x == 0) return 0;
+        if (x < 0) throw new ArgumentException("Must be non negative");
+        var invert = FastInvSqrt(x);
+        var result = Math.Truncate(1 / invert);
+        return (int)result;
+    }
+
+    private static int IterationSqrt(int x)
+    {
+        if (x < 0) throw new ArgumentException("Must be non negative");
+        if (x == 0) return 0;
+        if (x == 1) return x;
+        for (int i = 0; i <= x / 2 + 1; i++)
+        {
+            uint checkSqrt = (uint)(i * i);
+            if (checkSqrt == x) return i;
+            if (checkSqrt > x) return i - 1;
+        }
+        return 0;
+    }
+
+    private static int BinarySearchSqrt(int x)
+    {
+        if (x < 0) throw new ArgumentException("Must be non negative");
+        if (x == 0 || x == 1) return x;
+
+        int left = 1;
+        int right = x/2+1;
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            int check = x / mid;
+
+            if (check == mid) return mid;
+            if (check < mid) right = mid - 1;
+            else left = mid + 1;
+        }
+        return right;
+    }
+
+    public int MySqrt(int x)
+        => BinarySearchSqrt(x);
+    //=> IterationSqrt(x);
+}
---
+++ CsharpLeetCode.Tests/Easy/SqrtTests.cs
@@ -0,0 +1,88 @@
+﻿using CsharpLeetCode.Main.Easy;
+
+namespace CsharpLeetCode.Tests.Easy;
+public class SqrtTests
+{
+    private readonly Sqrt _sqrt = new();
+    [Fact]
+    public void CaseN1()
+    {
+        var x = 4;
+        var expected = 2;
+        var result = _sqrt.MySqrt(x);
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void CaseN2()
+    {
+        var x = 8;
+        var expected = 2;
+        var result = _sqrt.MySqrt(x);
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void CaseN3()
+    {
+        var x = 16;
+        var expected = 4;
+        var result = _sqrt.MySqrt(x);
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void CaseN4()
+    {
+        var x = 37;
+        var expected = 6;
+        var result = _sqrt.MySqrt(x);
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void CaseN5()
+    {
+        var x = 2147395599;
+        var expected = 46339;
+        var result = _sqrt.MySqrt(x);
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void CaseN6()
+    {
+        var x = 807810077;
+        var expected = 28421;
+        var result = _sqrt.MySqrt(x);
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void CaseN7()
+    {
+        var x = 1464898982;
+        var expected = 38273;
+        var result = _sqrt.MySqrt(x);
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void CaseN8()
+    {
+        var x = 2147395600;
+        var expected = 46340;
+        var result = _sqrt.MySqrt(x);
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void CaseN9()
+    {
+        var kek = 2147488281;
+        var x = 2147483647;
+        var expected = 46340;
+        var result = _sqrt.MySqrt(x);
+        Assert.Equal(expected, result);
+    }
+}
