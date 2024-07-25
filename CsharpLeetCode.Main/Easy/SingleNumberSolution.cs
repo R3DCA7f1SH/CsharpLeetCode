153776b4ec8a69d184aa754b61549ad1e44f425a
R3DCA7f1SH <mikhailodinol@gmail.com>
2024-07-25 19:45:24 +0700

    SortArray+SingleNumber

---
+++ CsharpLeetCode.Main/Easy/SingleNumberSolution.cs
@@ -0,0 +1,10 @@
+﻿namespace CsharpLeetCode.Main.Easy;
+public class SingleNumberSolution
+{
+    public int SingleNumber(int[] nums)
+    {
+        var result = nums[0];
+        for (int i = 1; i < nums.Length; i++) result ^= nums[i];
+        return result;
+    }
+}
---
+++ CsharpLeetCode.Main/Medium/SortArrayNlogn.cs
@@ -0,0 +1,26 @@
+﻿namespace CsharpLeetCode.Main.Medium;
+public class SortArrayNlogn
+{
+    public int[] SortArray(int[] nums)
+    {
+        if (nums.Length == 1) return nums;
+        var middle = nums.Length / 2;
+        var left = new int[middle];
+        for (int i = 0; i < middle; i++) left[i] = nums[i];
+        var right = new int[nums.Length - middle];
+        for (int i = 0; i < nums.Length - middle; i++) right[i] = nums[i + middle];
+        left = SortArray(left);
+        right = SortArray(right);
+        int leftptr = 0;
+        int rightptr = 0;
+        int[] sorted = new int[nums.Length];
+        for(int i = 0; i < nums.Length; i++)
+        {
+            var first = rightptr == right.Length || ((leftptr < left.Length) && (left[leftptr] <= right[rightptr]));
+            var second = leftptr == left.Length || ((rightptr < right.Length) && (right[rightptr] <= left[leftptr]));
+            if (first) sorted[i] = left[leftptr++];
+            else if(second) sorted[i] = right[rightptr++];
+        }
+        return sorted;
+    }
+}
