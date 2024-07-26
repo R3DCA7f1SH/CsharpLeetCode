2310cd7482ef64a75eeef9c0cc554e7abc7fb4c0
R3DCA7f1SH <mikhailodinol@gmail.com>
2024-07-26 12:43:27 +0700

    hamming weight binary

---
+++ CsharpLeetCode.Main/Easy/NumberOfОneBits.cs
@@ -0,0 +1,14 @@
+﻿namespace CsharpLeetCode.Main.Easy;
+public class NumberOfОneBits
+{
+    public int HammingWeight(int n)
+    {
+        int weight = 0;
+        while (n > 0)
+        {
+            weight += (n & 1); // Побитовое умножение на 1 равняется взятию остатка от деления на 2
+            n >>= 1; // Сдвиг влево равняется делению на 2
+        }
+        return weight;
+    }
+}
