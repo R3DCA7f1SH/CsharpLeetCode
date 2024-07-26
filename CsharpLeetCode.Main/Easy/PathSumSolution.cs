8a603f06b6e8ca861404bf99ea2bdf176dd156f1
R3DCA7f1SH <mikhailodinol@gmail.com>
2024-07-26 12:18:38 +0700

    PathSum

---
+++ CsharpLeetCode.Main/Easy/PathSumSolution.cs
@@ -0,0 +1,12 @@
+﻿using CsharpLeetCode.Main.Medium.DataStructure;
+
+namespace CsharpLeetCode.Main.Easy;
+public class PathSumSolution
+{
+    public bool HasPathSum(TreeNode root, int targetSum)
+    {
+        if(root == null) return false;
+        if (root.left == null && root.right == null) return root.val == targetSum;
+        return HasPathSum(root.left, targetSum - root.val) || HasPathSum(root.right, targetSum - root.val);
+    }
+}
