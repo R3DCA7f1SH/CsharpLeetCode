f55f20859d791486e14ad0c7bceebe3db93b0803
R3DCA7f1SH <mikhailodinol@gmail.com>
2024-07-26 12:05:18 +0700

    HappyNumber

---
+++ CsharpLeetCode.Main/Easy/HappyNumberSolution.cs
@@ -0,0 +1,25 @@
+﻿namespace CsharpLeetCode.Main.Easy;
+public class HappyNumberSolution
+{
+    public bool IsHappy(int n)
+    {
+        if (n == 1 || n == 7) return true;
+        var current = n;
+        var count = 0;
+        while (true)
+        {
+            var sumQuad = 0;
+            while (current > 0 )
+            {
+                var number = current % 10;
+                sumQuad += number*number;
+                current /= 10;
+            }
+            if(sumQuad<10) count++;
+            current = sumQuad;
+            if (current == 1 || current == 7) return true;
+            if (count > 1) return false;
+        }
+    }
+}
+
---
+++ CsharpLeetCode.Tests/Easy/HappyNumberTests.cs
@@ -0,0 +1,35 @@
+﻿using CsharpLeetCode.Main.Easy;
+
+namespace CsharpLeetCode.Tests.Easy;
+
+public class HappyNumberTests
+{
+    private readonly HappyNumberSolution _isHappy = new();
+    
+    [Fact]
+    public void CaseN1()
+    {
+        var parameter = 19;
+        var expected = true;
+        var result = _isHappy.IsHappy(parameter);
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void CaseN2()
+    {
+        var parameter = 2;
+        var expected = false;
+        var result = _isHappy.IsHappy(parameter);
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void CaseN3()
+    {
+        var parameter = 11;
+        var expected = false;
+        var result = _isHappy.IsHappy(parameter);
+        Assert.Equal(expected, result);
+    }
+}
